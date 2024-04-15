a2f3540ad50a67bc5cc356a7c90185c7d308d5ad
iNikolas <realkolos1@gmail.com>
2024-04-15 11:08:45 +0300

    cache response feature

--- src/command-handler/command-handler.service.ts
+++ src/command-handler/command-handler.service.ts
@@ -1,67 +1,76 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { SerialPortService } from '../serial-port/serial-port.service';
 import { ConfigService } from '@nestjs/config';
+
+import { SerialPortService } from '../serial-port/serial-port.service';
 
 @Injectable()
 export class CommandHandlerService {
-  private timeout: NodeJS.Timeout;
+  private cachedResponse: string;
+  private cacheExpirationMs: number;
+  private cacheUpdatedTimestamp: number;
 
   constructor(
     private readonly serialPortService: SerialPortService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    this.cacheExpirationMs = parseInt(
+      this.configService.get<string>('CACHE_EXPIRATION_MS'),
+    );
+  }
 
   async sendCommand(command: any): Promise<string> {
+    if (this.cachedResponse && !this.isCacheExpired()) {
+      return this.cachedResponse;
+    }
+
     try {
-      return await this.sendCommandWithTimeout(
-        command,
-        parseInt(this.configService.get<string>('TIMEOUT_MS')),
-      );
-    } catch (error) {
-      console.error('Error sending command:', error.message);
-      throw new HttpException(
-        'Failed to send command',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      const response = await this.writeCommand(command);
+      this.updateCache(response);
+      return response;
+    } catch (errorMessage) {
+      throw new HttpException(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   private async writeCommand(command: any): Promise<string> {
     return new Promise((resolve, reject) => {
+      const timeout = setTimeout(
+        () => {
+          this.serialPortService.initPort();
+          reject('No response from the Board. Please retry.');
+        },
+        parseInt(this.configService.get<string>('TIMEOUT_MS')),
+      );
+
       this.serialPortService.port.write(JSON.stringify(command), (err) => {
+        clearTimeout(timeout);
         if (err) {
-          reject(err);
+          reject(err.message);
         }
       });
 
       this.serialPortService.port.once('data', (data) => {
+        clearTimeout(timeout);
         const jsonData = data.toString('utf-8');
         resolve(jsonData);
       });
     });
   }
 
-  private async sendCommandWithTimeout(
-    command: any,
-    timeoutMs: number,
-  ): Promise<string> {
-    this.timeout = setTimeout(() => {
-      this.serialPortService.initPort();
-      const errorMessage = 'Timeout occurred while waiting for response';
-      console.log(errorMessage);
-      throw new HttpException(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-    }, timeoutMs);
+  private updateCache(response: string) {
+    this.cachedResponse = response;
+    this.cacheUpdatedTimestamp = Date.now();
+    setTimeout(() => {
+      this.cachedResponse = null;
+    }, this.cacheExpirationMs);
+  }
 
-    try {
-      const responseData = await this.writeCommand(command);
-      clearTimeout(this.timeout);
-      return responseData;
-    } catch (error) {
-      clearTimeout(this.timeout);
-      throw new HttpException(
-        'Failed to send command',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  private isCacheExpired(): boolean {
+    const cacheUpdatedTimestamp = this.cacheUpdatedTimestamp ?? 0;
+    return (
+      !this.cachedResponse ||
+      !this.cacheExpirationMs ||
+      Date.now() > cacheUpdatedTimestamp + this.cacheExpirationMs
+    );
   }
 }
